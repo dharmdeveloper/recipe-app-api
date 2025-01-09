fc41b541ff74db668aea1ec7cda564ee1dec9e6d
dharmpatel05 <jacques@duoveo.com>
2025-01-09 08:49:53 +0530

    Changes

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,9 @@
-""" Calculator function"""
+"""Calculator functions"""
 
-def add(x , y):
-
-    
-    """Add x and y """
+def add(x, y):
+    """Add x and y"""
     return x + y
+
 def subtract(x, y):
-
-
-    """Subtract y from x """
+    """Subtract y from x"""
     return x - y
